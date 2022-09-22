550b5b91dca7ebd4ed2e042ac24ee1f157d7f7cb
Christoph Rist <c.rist@posteo.de>
2022-09-22 21:40:44 +0000

    Add gphotos-sync main to listener

--- gphotos-sync-listener/main.py
+++ gphotos-sync-listener/main.py
@@ -1,28 +1,34 @@
+import logging
 import asyncio, socket
+
+from gphotos_sync.Main import GooglePhotosSyncMain
+
+logging.basicConfig(level=logging.INFO)
 
 my_task = None
 
 async def some_long_running_task():
     print("start")
-    await asyncio.sleep(3)
-    print("end")
+    logging.info("Starting gphotos-sync task")
+    GooglePhotosSyncMain().main(["/storage"])
 
 async def handle_client(reader, writer):
-    print(type(reader))
     global my_task
-    request = None
-    while request != 'quit':
-        request = (await reader.read(-1)).decode('utf8')
-        print("bla")
-        print(request)
+    request = (await reader.read(-1)).decode('utf8')
+    logging.info(f"Got the request {request}")
+    if request == "sync":
         if my_task is not None and not my_task.done():
-            response = "still running\n"
+            logging.info(f"Task still running.")
+            response = b'\x02'
         else:
+            logging.info(f"Starting task.")
+            response = b'\x00'
             my_task = asyncio.create_task(some_long_running_task())
-            response = "got it\n"
-        writer.write(response.encode('utf8'))
-        await writer.drain()
-        break
+    else:
+        logging.info(f"Unknown command.")
+        response = b'\x01'
+    writer.write(response)
+    await writer.drain()
     writer.close()
 
 async def run_server():
