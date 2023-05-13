b7af8149f1a28c7645810000d699f0a34f3eb87a
Christoph Rist <c.rist@posteo.de>
2023-05-13 15:00:46 +0000

    [Duplicati] Update btrfs snapshot helpers to support volume names

--- duplicati/scripts/snapshot.sh
+++ duplicati/scripts/snapshot.sh
@@ -2,13 +2,17 @@
 set -eu
 
 function create_snapshot {
+  local arg1_src_name="$1"
+  # default: same name in destination
+  local arg2_dest_name="${2:-$arg1_src_name}"
+
   # use a lockfile to guard access to the pipe (in/out)
   lockfile="/pipe/pipe.lockfile"
   exec 3>$lockfile
   flock -x 3
 
   # create snapshot and exit from this function with the desired return value
-  echo "create $1" > /pipe/btrfs_pipe_in
+  echo "create $arg1_src_name $arg2_dest_name" > /pipe/btrfs_pipe_in
   retval=$(cat /pipe/btrfs_pipe_out)
   exit $retval
 }
