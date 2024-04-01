e1fe507635b08f61dddb2dda04c79c0abce49547
Christoph Rist <c.rist@posteo.de>
2024-04-01 18:25:04 +0000

    [ddclient] Update for cloudflare

--- ddclient/image/ddclient_entrypoint.sh
+++ ddclient/image/ddclient_entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 cp /config/ddclient.conf /etc/ddclient.conf
 
-while true; do
-  /usr/sbin/ddclient -verbose
-  # if update did not return 0, exit with status code 1
-  if [ $? -ne 0 ]; then
-    echo 'errored out, exiting'
-    exit 1
-  fi
-  sleep 1800
-done
+# config starts a daemon that checks and updates periodically
+/usr/sbin/ddclient -verbose
+
+# wait forever
+trap : TERM INT; sleep infinity & wait
+
