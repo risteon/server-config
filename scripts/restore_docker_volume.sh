95367124f93a2a14bc191d0971a8399c582f80a6
Christoph Rist <c.rist@posteo.de>
2024-04-07 16:03:43 +0000

    [scripts] Improve docker restore

--- scripts/restore_docker_volume.sh
+++ scripts/restore_docker_volume.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+set -eux
+
+# usage: ./restore_docker_volume.sh VOLUME_NAME PATH_TO_TAR_FILE
+
+DIR="$(dirname "${2}")" ; FILE="$(basename "${2}")"
+
+cd $DIR
+
 docker run --rm \
   -v "$1":/backup-volume \
   -v "$(pwd)":/backup:ro \
   busybox \
-  tar --directory /backup-volume -xvf "/backup/backup_$1.tar.gz"
+  tar --directory /backup-volume -xf "/backup/$FILE"
